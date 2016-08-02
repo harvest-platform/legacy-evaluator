ba5fdbdced9ab885c8ec20f30a907d2ddd6a2269
Byron Ruth <b@devel.io>
2016-08-02 06:36:10 -0400

    Split up business logic from server boilerplate
    Fix #1
    
    Signed-off-by: Byron Ruth <b@devel.io>

---
+++ methods.py
@@ -0,0 +1,169 @@
+import re
+from avocado.query import oldparsers as parsers
+from avocado.models import DataConcept
+from avocado.query.operators import registry as operators
+
+
+# Operator mapping.
+op_map_in = {
+    'eq': 'exact',
+    '-eq': '-exact',
+}
+
+op_map_out = {
+    'exact': 'eq',
+    '-exact': '-eq',
+}
+
+op_set = {'range', 'exact', 'gt', 'lt', 'gte', 'lte', 'in'}
+
+punc_re = re.compile(r'[\/\'",]+')
+space_re = re.compile(r'\s+')
+
+
+def catalog():
+    concepts_query = DataConcept.objects.published().filter(queryable=True)
+    concepts = []
+
+    for c in concepts_query:
+        params = {}
+
+        for cf in c.concept_fields.select_related('field'):
+            f = cf.field
+
+            ops = {}
+            for k, _ in f.operators:
+                o = operators.get(k)
+
+                if o.lookup not in op_set:
+                    continue
+
+                k = op_map_out.get(k, k)
+                ops[k] = {
+                    'id': o.lookup,
+                    'doc': '{} ({})'.format(o.verbose_name, o.short_name),
+                    'multiple': hasattr(o, 'join_string'),
+                }
+
+            key = '{}.{}.{}'.format(*f.natural_key())
+
+            params[key] = {
+                'id': f.id,
+                'label': str(cf),
+                'type': f.simple_type,
+                'doc': f.description,
+                'nullable': f.nullable,
+                'operators': ops,
+            }
+
+        local = space_re.sub('_', punc_re.sub('', c.name.lower()))
+
+        concepts.append({
+            'id': c.pk,
+            'uri': 'audgendb.{}'.format(local),
+            'label': str(c),
+            'doc': c.description,
+            'keywords': c.keywords,
+            'type': None,
+            'params': params,
+        })
+
+    return {
+        'version': '1.0.0',
+        'concept': concepts,
+    }
+
+
+def validate(expr):
+    error = None
+    context = translate_expr(expr)
+
+    try:
+        parsers.datacontext.parse(context)
+    except Exception as e:
+        error = str(e)
+
+    return error
+
+
+def plan(expr):
+    context = translate_expr(expr)
+    node = parsers.datacontext.parse(context)
+
+    queryset = node.apply().values_list('pk', flat=True).order_by()
+    compiler = queryset.query.get_compiler(queryset.db)
+    sql, params = compiler.as_sql()
+
+    return {
+        'harvest': context,
+        'sql': sql,
+        'params': params,
+    }
+
+
+def idents(expr):
+    context = translate_expr(expr)
+    node = parsers.datacontext.parse(context)
+
+    queryset = node.apply().values_list('pk', flat=True).order_by()
+    compiler = queryset.query.get_compiler(queryset.db)
+
+    return tuple(r[0] for r in compiler.results_iter())
+
+
+def count(expr):
+    context = translate_expr(expr)
+    node = parsers.datacontext.parse(context)
+    return node.apply().count()
+
+
+def translate_expr(expr):
+    return translate_term(expr['term'])
+
+
+def translate_term(term):
+    # Corresponds to a branch.
+    if term['type'] == 'branch':
+        return {
+            'type': term['operator'],
+            'children': map(translate_term, term['terms']),
+        }
+
+    # Corresponds to a set of query conditions that are ANDed together.
+    elif term['type'] == 'leaf':
+        # Wrap in a branch node.
+        preds = []
+
+        for p in term['params'].values():
+            op, val = translate_op(p['operator'], p['value'])
+
+            preds.append({
+                'concept': term['id'],
+                'field': p['id'],
+                'operator': op,
+                'value': val,
+            })
+
+        return {
+            'type': 'and',
+            'children': preds,
+        }
+
+
+def translate_op(op, val):
+    if op in op_map_in:
+        op = op_map_in[op]
+
+    if op == 'range':
+        if 'gt' in val and 'lt' in val:
+            return op, (val['gt'], val['lt'])
+
+        if 'gt' in val:
+            return 'gt', val['gt']
+
+        if 'lt' in val:
+            return 'lt', val['lt']
+
+        raise ValueError('invalid range')
+
+    return op, val
--- service.py
+++ service.py
@@ -1,30 +1,11 @@
-import re
 import json
 from flask import Flask, Response, request
-from avocado.query import oldparsers as parsers
-from avocado.models import DataConcept
-from avocado.query.operators import registry as operators
+import methods
 
 
 app = Flask(__name__)
 host = None
 port = None
-
-# Operator mapping.
-op_map_in = {
-    'eq': 'exact',
-    '-eq': '-exact',
-}
-
-op_map_out = {
-    'exact': 'eq',
-    '-exact': '-eq',
-}
-
-op_set = {'range', 'exact', 'gt', 'lt', 'gte', 'lte', 'in'}
-
-punc_re = re.compile(r'[\/\'",]+')
-space_re = re.compile(r'\s+')
 
 
 @app.route('/', methods=['GET'])
@@ -34,57 +15,7 @@ def root():
 
 @app.route('/catalog', methods=['GET'])
 def catalog():
-    concepts_query = DataConcept.objects.published().filter(queryable=True)
-    concepts = []
-
-    for c in concepts_query:
-        params = {}
-
-        for cf in c.concept_fields.select_related('field'):
-            f = cf.field
-
-            ops = {}
-            for k, _ in f.operators:
-                o = operators.get(k)
-
-                if o.lookup not in op_set:
-                    continue
-
-                k = op_map_out.get(k, k)
-                ops[k] = {
-                    'id': o.lookup,
-                    'doc': '{} ({})'.format(o.verbose_name, o.short_name),
-                    'multiple': hasattr(o, 'join_string'),
-                }
-
-            key = '{}.{}.{}'.format(*f.natural_key())
-
-            params[key] = {
-                'id': f.id,
-                'label': str(cf),
-                'type': f.simple_type,
-                'doc': f.description,
-                'nullable': f.nullable,
-                'operators': ops,
-            }
-
-        local = space_re.sub('_', punc_re.sub('', c.name.lower()))
-
-        concepts.append({
-            'id': c.pk,
-            'uri': 'audgendb.{}'.format(local),
-            'label': str(c),
-            'doc': c.description,
-            'keywords': c.keywords,
-            'type': None,
-            'params': params,
-        })
-
-    resp = Response(json.dumps({
-        'version': '1.0.0',
-        'concept': concepts,
-    }))
-
+    resp = Response(json.dumps(methods.catalog()))
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
@@ -92,14 +23,7 @@ def catalog():
 @app.route('/validate', methods=['POST'])
 def validate():
     expr = request.get_json()
-
-    error = None
-    context = translate_expr(expr)
-
-    try:
-        parsers.datacontext.parse(context)
-    except Exception as e:
-        error = str(e)
+    error = methods.validate(expr)
 
     if error:
         return json.dumps({
@@ -112,22 +36,9 @@ def validate():
 @app.route('/plan', methods=['POST'])
 def plan():
     expr = request.get_json()
+    plan = methods.plan(expr)
 
-    context = translate_expr(expr)
-    node = parsers.datacontext.parse(context)
-
-    queryset = node.apply().values_list('pk', flat=True).order_by()
-    compiler = queryset.query.get_compiler(queryset.db)
-    sql, params = compiler.as_sql()
-
-    resp = Response(json.dumps({
-        'plan': {
-            'harvest': context,
-            'sql': sql,
-            'params': params,
-        },
-    }))
-
+    resp = Response(json.dumps(plan))
     resp.headers['Content-Type'] = 'application/json'
 
     return resp
@@ -136,15 +47,9 @@ def plan():
 @app.route('/idents', methods=['POST'])
 def idents():
     expr = request.get_json()
+    idents = methods.idents(expr)
 
-    context = translate_expr(expr)
-    node = parsers.datacontext.parse(context)
-
-    queryset = node.apply().values_list('pk', flat=True).order_by()
-    compiler = queryset.query.get_compiler(queryset.db)
-    ids = tuple(r[0] for r in compiler.results_iter())
-
-    resp = Response(json.dumps(ids))
+    resp = Response(json.dumps(idents))
     resp.headers['Content-Type'] = 'application/json'
 
     return resp
@@ -153,71 +58,12 @@ def idents():
 @app.route('/count', methods=['POST'])
 def count():
     expr = request.get_json()
+    count = methods.count(expr)
 
-    context = translate_expr(expr)
-
-    node = parsers.datacontext.parse(context)
-    count = node.apply().count()
-
-    resp = Response(json.dumps({
-        'count': count,
-    }))
-
+    resp = Response(json.dumps({'count': count}))
     resp.headers['Content-Type'] = 'application/json'
 
     return resp
-
-
-def translate_expr(expr):
-    return translate_term(expr['term'])
-
-
-def translate_term(term):
-    # Corresponds to a branch.
-    if term['type'] == 'branch':
-        return {
-            'type': term['operator'],
-            'children': map(translate_term, term['terms']),
-        }
-
-    # Corresponds to a set of query conditions that are ANDed together.
-    elif term['type'] == 'leaf':
-        # Wrap in a branch node.
-        preds = []
-
-        for p in term['params'].values():
-            op, val = translate_op(p['operator'], p['value'])
-
-            preds.append({
-                'concept': term['id'],
-                'field': p['id'],
-                'operator': op,
-                'value': val,
-            })
-
-        return {
-            'type': 'and',
-            'children': preds,
-        }
-
-
-def translate_op(op, val):
-    if op in op_map_in:
-        op = op_map_in[op]
-
-    if op == 'range':
-        if 'gt' in val and 'lt' in val:
-            return op, (val['gt'], val['lt'])
-
-        if 'gt' in val:
-            return 'gt', val['gt']
-
-        if 'lt' in val:
-            return 'lt', val['lt']
-
-        raise ValueError('invalid range')
-
-    return op, val
 
 
 if __name__ == '__main__':
